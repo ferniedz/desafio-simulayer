7b24a95f7924637667196d54e63cb79c3d30331c
ferniedz <ferniedzwiedki@outlook.com>
2026-04-20 20:51:06 -0300

    feat: server.js criado e funcionando

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, async() => {
+    console.log("O servidor está online e rodando na porta", PORT);
+});
